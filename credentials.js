38de45f6dbd7cdfc915685182d60224874b38bf1
root <root@umc-console.lcmchealth.org>
2022-07-08 17:17:01 -0500

    sample of new credentials structure

--- credentials.js
+++ credentials.js
@@ -1,34 +1,42 @@
 var snmp = require ("net-snmp");
 
-var credentials = [
-    {
-        version: 3,
-        options: {
-            port: 161,
-            retries: 1,
-            timeout: 5000,
-            transport: "udp4",
-            trapPort: 162,
-            version: snmp.Version3,
-            engineID: "8000B98380XXXXXXXXXXXXXXXXXXXXXXXX", // where the X's are random hex digits
-            backwardsGetNexts: true,
-            reportOidMismatchErrors: false,
-            idBitsSize: 32,
-            context: ""
+var credentials = {
+    snmp: [
+        {
+            version: 3,
+            options: {
+                port: 161,
+                retries: 1,
+                timeout: 5000,
+                transport: "udp4",
+                trapPort: 162,
+                version: snmp.Version3,
+                engineID: "8000B98380XXXXXXXXXXXXXXXXXXXXXXXX", // where the X's are random hex digits
+                backwardsGetNexts: true,
+                reportOidMismatchErrors: false,
+                idBitsSize: 32,
+                context: ""
+            },
+            user: {
+                name: "mysnmpuser",
+                level: snmp.SecurityLevel.authPriv,
+                authProtocol: snmp.AuthProtocols.sha,
+                authKey: "mysnmpauthkey",
+                privProtocol: snmp.PrivProtocols.aes256r,
+                privKey: "mysnmpprivkey"
+            }
         },
-        user: {
-            name: "mysnmpuser",
-            level: snmp.SecurityLevel.authPriv,
-            authProtocol: snmp.AuthProtocols.sha,
-            authKey: "myauthkey",
-            privProtocol: snmp.PrivProtocols.aes256r,
-            privKey: "myprivkey"
+        {
+            version: 2,
+            community: "public"
         }
-    },
-    {
-        version: 2,
-        community: "public"
-    }
-]
+    ],
+    ssh: [
+        {
+            username: 'mysshusername',
+            password: 'mysshpassword'
+        }
+    ]
+}
 
 module.exports = credentials;
