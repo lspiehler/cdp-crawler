ccdaa41690ff78904a228eef2718d4dfec349c46
root <root@umc-console.lcmchealth.org>
2022-07-13 07:00:13 -0500

    added options to config

--- config.js
+++ config.js
@@ -12,7 +12,23 @@ var parseArray = function(str) {
     }
 }
 
+function getBoolean(str) {
+	if(str) {
+		if(str.toUpperCase()=='TRUE') {
+			return true;
+		} else if(str.toUpperCase()=='FALSE') {
+			return false;
+		} else {
+			return str;
+		}
+	} else {
+		return false;
+	}
+}
+
 module.exports = {
     seeds: parseArray(process.env.SEEDS) || [],
-    exclusions: parseArray(process.env.PLATFORMEXCLUSIONS) || []
+    exclusions: parseArray(process.env.PLATFORMEXCLUSIONS) || [],
+    getexcluded: getBoolean(process.env.GETEXCLUDED) || false,
+    tacacsuser: process.env.TACACSUSER || false
 }
